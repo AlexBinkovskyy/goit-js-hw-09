92a743dd2306ad2e60aa035d018f1ecd8761a7d3
AlexBinkovskyy <alex_bin@ukr.net>
2023-11-26 23:52:43 +0200

    edit task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,7 +3,17 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const inputField = document.querySelector('#datetime-picker');
 let startBtn = document.querySelector('[data-start]');
+startBtn.addEventListener('click', onClick);
+const daysRemain = document.querySelector('[data-days]');
+const hoursRemain = document.querySelector('[data-hours]');
+const minutesRemain = document.querySelector('[data-minutes]');
+const secondsRemain = document.querySelector('[data-seconds]');
 startBtn.disabled = true;
+
+function onClick() {
+  const checkedDate = JSON.parse(localStorage.getItem('checkedDate'));
+  startCountdown(checkedDate);
+}
 
 const options = {
   enableTime: true,
@@ -11,59 +21,59 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    checkDate(selectedDates);
+    const checkedDate = checkDate(selectedDates[0]);
+    localStorage.setItem('checkedDate', JSON.stringify(checkedDate.getTime()));
   },
 };
 
 flatpickr('#datetime-picker', options);
 
 function checkDate(selectedDates) {
-  const date = new Date();
-  const currentMinutes = date.getHours();
-  const currentHours = date.getMinutes();
-  const currentDate = date.getDate();
-  const currentMonth = date.getMonth();
-  const currentYear = date.getFullYear();
+  const date = new Date().getTime();
+  selectedDates.getTime() > date
+    ? (startBtn.disabled = false)
+    : ((startBtn.disabled = true), alert('Please choose a date in the future'));
+  return selectedDates;
+}
 
-  const selectedMinutes = selectedDates[0].getHours();
-  const selectedHours = selectedDates[0].getMinutes();
-  const selectedDate = selectedDates[0].getDate();
-  const selectedMonth = selectedDates[0].getMonth();
-  const selectedYear = selectedDates[0].getFullYear();
+function startCountdown(checkedDate) {
+  createLayout(checkedDate);
+  setInterval(() => {
+    createLayout(checkedDate);
+  }, 1000);
+}
 
-  if (
-    selectedYear <= currentYear &&
-    selectedMonth <= currentMonth &&
-    selectedDate <= currentDate &&
-    selectedHours <= currentHours &&
-    selectedMinutes <= currentMinutes
-  ) {
-    startBtn.disabled = true;
-    alert('Please choose a date in the future')
+function createLayout(checkedDate) {
+  startBtn.disabled = true;
+  let curDate = new Date();
+  curDate = curDate.getTime();
+  const result = checkedDate - curDate;
+  if (result >= 0) {
+    const remainTime = convertMs(result);
+    daysRemain.textContent = `${remainTime.days.toString().padStart(2, '0')}`;
+    hoursRemain.textContent = `${remainTime.hours.toString().padStart(2, '0')}`;
+    minutesRemain.textContent = `${remainTime.minutes.toString().padStart(2, '0')}`;
+    secondsRemain.textContent = `${remainTime.seconds.toString().padStart(2, '0')}`;
   } else {
-    startBtn.disabled = false;
+    return;
   }
 }
 
 // function addLeadingZero(value){
-//     padStart()
+//     padStart(2, 0)
+//     return value
 // }
 
-// function convertMs(ms) {
-//     // Number of milliseconds per unit of time
-//     const second = 1000;
-//     const minute = second * 60;
-//     const hour = minute * 60;
-//     const day = hour * 24;
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-//     // Remaining days
-//     const days = Math.floor(ms / day);
-//     // Remaining hours
-//     const hours = Math.floor((ms % day) / hour);
-//     // Remaining minutes
-//     const minutes = Math.floor(((ms % day) % hour) / minute);
-//     // Remaining seconds
-//     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-//     return { days, hours, minutes, seconds };
-//   }
+  return { days, hours, minutes, seconds };
+}
